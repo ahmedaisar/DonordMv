544f53a67f2fdc77dbf75acf0e969337b61b7aef
Ali Nihadh <nihadh27@hotmail.com>
2019-10-26 02:05:35 +0500

    Corrected password hash and boolean return values

--- donorsmv/files/functions.php
+++ donorsmv/files/functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //check if this phone number is registered or not and returns true if exists or false otherwise.
 function person_exists($phone) {
@@ -43,14 +43,14 @@ function get_age ($dob) {
 
 //adds a non-user personal information to the DB.
 function add_person ($phone, $fname, $mname, $lname, $bloodgroup, $gender, $dob) {
-	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');	
-	$sql = "INSERT IGNORE INTO donors (`phone`,`fname`,`mname`,`lname`,`bloodGroup`,`gender`,`DOB`)" 
+	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');
+	$sql = "INSERT IGNORE INTO donors (`phone`,`fname`,`mname`,`lname`,`bloodGroup`,`gender`,`DOB`)"
 	. "VALUES ('$phone','$fname','$mname','$lname','$bloodgroup','$gender','$dob')";
 		if ($connection->query($sql) === TRUE) {
-			return "TRUE";
+			return TRUE;
 		} else {
-			return "FALSE";
-		}	
+			return FALSE;
+		}
 }
 
 
@@ -58,7 +58,7 @@ function add_person ($phone, $fname, $mname, $lname, $bloodgroup, $gender, $dob)
 function register_user ($email, $phone, $password){
 	$timestamp = date('Y-m-d',strtotime('now'));
 	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');
-	$sql = "INSERT IGNORE INTO users (`email`,`user`,`pword`,`timeStamp`)" 
+	$sql = "INSERT IGNORE INTO users (`email`,`user`,`pword`,`timeStamp`)"
 	. "VALUES ('$email', '$phone', '$password', '$timestamp')";
 		if ($connection->query($sql) === TRUE) {
 			return TRUE;
@@ -70,17 +70,17 @@ function register_user ($email, $phone, $password){
 //encripts password
 function passwordify($email, $pass) {
 	$email = hash('sha256', $email);
-    $pass = hash('sha256', $email);
-    $pass =  $email . $pass;
-    return $pass = hash('sha256', $pass);
+  $pass = hash('sha256', $pass);
+  $pass =  $email . $pass;
+  return $pass = hash('sha256', $pass);
 }
 
 //checks if password and confirm password is matching
 function match_passwords ($pass, $cpass) {
 	if ($pass === $cpass) {
-		return 'TRUE';
+		return TRUE;
 	} else {
-		return 'FALSE';
+		return FALSE;
 	}
 
 }
@@ -92,7 +92,7 @@ function varify_password ($email, $pass) {
 	$result = mysqli_query($connection, $sql);
 	while ($data = mysqli_fetch_object($result)) {
 	$pword = $data -> pword;
-	
+
 	if ($pass === $pword) {
         return TRUE;
 	} else {
@@ -123,7 +123,7 @@ function get_all_donors (){
 			break;
 			case '0':
 				$gender = "Female";
-			break;	
+			break;
 
 			default:
 				$gender = "N/A";
@@ -132,7 +132,7 @@ function get_all_donors (){
 //output a table row
 echo
 "<tr><td>" . $phone . "</td><td>" . $fullname . "</td><td>" . $bloodGroup . "</td><td>" . $gender . "</td><td>" . $age . "</td></tr>";
-} 
+}
 }
 
 
@@ -158,7 +158,7 @@ function get_donors_by_bloodgroup ($bloodgroup){
 			break;
 			case '0':
 				$gender = "Female";
-			break;	
+			break;
 
 			default:
 				$gender = "N/A";
@@ -167,7 +167,7 @@ function get_donors_by_bloodgroup ($bloodgroup){
 //output a table row
 echo
 "<tr><td>" . $phone . "</td><td>" . $fullname . "</td><td>" . $bloodGroup . "</td><td>" . $gender . "</td><td>" . $age . "</td></tr>";
-} 
+}
 }
 
 //creates a session with id of email and logs the user in
@@ -181,8 +181,8 @@ function login($email, $pass){
 			return FALSE;
 		}
 }
- 
-//gets an array with the user data   
+
+//gets an array with the user data
 function load_profile ($email){
 	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');
 	$sql = "SELECT * FROM people WHERE email = '$email'";
@@ -201,7 +201,7 @@ function load_profile ($email){
 			break;
 			case '0':
 				$user['gender'] = "Female";
-			break;	
+			break;
 
 			default:
 				$user['gender'] = "N/A";
@@ -211,21 +211,21 @@ function load_profile ($email){
 		$user['dob'] = $data -> DOB;
 		$user['age'] = date_diff(date_create($user['dob']), date_create('now'))->y." yrs";
 		$user['fullname'] = trim($user['fname']." ".$user['mname']." ".$user['lname']);
-		
+
     	return $user;
 	}
 }
 
 //adds a new donation log to the DB.
 function add_donation ($donor, $location, $date, $notes) {
-	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');	
-	$sql = "INSERT IGNORE INTO donations (`donor`, `location`, `donationDate`, `notes`)" 
+	$connection = mysqli_connect('localhost', 'root', '', 'donorsdb');
+	$sql = "INSERT IGNORE INTO donations (`donor`, `location`, `donationDate`, `notes`)"
 	. "VALUES ('$donor', '$location', '$date', '$notes')";
 		if ($connection->query($sql) === TRUE) {
-			return "TRUE";
+			return TRUE;
 		} else {
-			return "FALSE";
-		}	
+			return FALSE;
+		}
 }
 
 //gets user's blood donation log if it exists
@@ -237,7 +237,7 @@ function get_donoation_log ($phone){
 		while ($data = mysqli_fetch_object($result)) {
 		$location = $data -> location;
 		$donationDate = date('d-M-Y', strtotime($data -> donationDate));
-		$notes = $data -> notes;		
+		$notes = $data -> notes;
 echo "<li class='collection-item avatar'>".
       "<i class='zmdi zmdi-invert-colors red circle'></i>".
       "<span class='title'><b>".$location."</b></span>".
@@ -253,7 +253,7 @@ echo "<li class='collection-item avatar'>".
 		} else {
 			echo "<li class='red-text center'>No donations recorded</li>";
 		}
-		 
+
 }
 
 //get the latest donation info for the phone number
